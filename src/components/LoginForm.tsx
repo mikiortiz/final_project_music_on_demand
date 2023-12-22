69097259671355bdb93a01a03f293175569392e5
miki <ortizmichel390@gmail.com>
2023-12-22 14:58:20 -0300

    Add Quick Access to Test Users

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,27 +1,49 @@
-import React from "react";
-import { Button, TextField, Typography, Grid } from "@mui/material";
-import { FormControl } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Typography,
+  Grid,
+  FormControl,
+  TextField,
+} from "@mui/material";
+import { Modal, Box } from "@mui/material";
 import { useFormik } from "formik";
 import * as yup from "yup";
 import { useSelector } from "react-redux";
 import { RootState } from "../model/RootStateTypes";
-import { SupplierData } from "../model/SupplierData";
-import { UserData } from "../model/UserData";
 import { useNavigate } from "react-router-dom";
+import logomusic from "/images/Logomusic.png";
+import IconButton from "@mui/material/IconButton";
+import RecentActorsIcon from "@mui/icons-material/RecentActors";
+import CloseIcon from "@mui/icons-material/Close";
 
 interface Props {
   onClose: () => void;
 }
 
+interface User {
+  userEmail: string;
+  userPassword: string;
+}
+
 const LoginForm: React.FC<Props> = ({ onClose }) => {
   const navigate = useNavigate();
 
-  const users = useSelector(
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+
+  const djUsers = useSelector((state: RootState) => state.registered.DjsUsers);
+  const musicUsers = useSelector(
     (state: RootState) => state.registered.MusicUsers
-  ) as UserData[];
-  const suppliers = useSelector(
-    (state: RootState) => state.registered.DjsUsers
-  ) as SupplierData[];
+  );
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   const formik = useFormik({
     initialValues: {
@@ -48,10 +70,10 @@ const LoginForm: React.FC<Props> = ({ onClose }) => {
       return;
     }
 
-    const user = users.find(
+    const supplier = djUsers.find(
       (u) => u.userEmail === email && u.userPassword === password
     );
-    const supplier = suppliers.find(
+    const user = musicUsers.find(
       (s) => s.userEmail === email && s.userPassword === password
     );
 
@@ -74,106 +96,287 @@ const LoginForm: React.FC<Props> = ({ onClose }) => {
     window.alert(mensaje);
   };
 
+  const handleUserSelection = (user: User) => {
+    setSelectedUser(user);
+    closeModal();
+  };
+
+  useEffect(() => {
+    if (selectedUser) {
+      formik.setValues({
+        email: selectedUser.userEmail,
+        password: selectedUser.userPassword,
+      });
+    }
+  }, [selectedUser]);
+
   return (
-    <Grid
-      item
-      xs={12}
-      style={{
-        marginTop: "-225px",
-        borderRadius: "10px",
-        position: "fixed",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "580px",
-        width: "900px",
-        zIndex: 9999,
-        backgroundColor: "rgba(0, 0, 0, 1)",
-      }}
-    >
+    <>
       <Grid
-        container
         item
-        xs={10}
-        sm={10}
-        md={10}
-        sx={{
+        xs={12}
+        style={{
+          marginTop: "-225px",
           borderRadius: "10px",
-          width: "550px",
-          height: "500px",
-          padding: "1rem",
+          position: "fixed",
           display: "flex",
-          flexDirection: "column",
+          justifyContent: "center",
           alignItems: "center",
-          position: "relative",
-          backgroundColor: "rgba(240, 240, 240, 1)",
+          height: "580px",
+          width: "900px",
+          zIndex: 9999,
+          backgroundImage: `url(${logomusic})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
         }}
       >
-        <Typography variant="h4" color="primary" gutterBottom>
-          Iniciar Sesión
-        </Typography>
-        <Grid sx={{ width: "90%" }}>
-          <form onSubmit={formik.handleSubmit}>
-            <FormControl style={{ width: "100%" }}>
-              <Typography>Email</Typography>
-              <TextField
-                variant="outlined"
-                fullWidth
-                size="small"
-                margin="normal"
-                type="email"
-                {...formik.getFieldProps("email")}
-                sx={{ backgroundColor: "white", marginBottom: "1rem" }}
-                error={
-                  formik.touched.email &&
-                  Boolean(formik.errors.email)
-                }
-                helperText={
-                  formik.touched.email && formik.errors.email
-                }
-              />
-              <Typography>Contraseña</Typography>
-              <TextField
-                variant="outlined"
-                fullWidth
-                size="small"
-                margin="normal"
-                type="password"
-                {...formik.getFieldProps("password")}
-                sx={{ backgroundColor: "white", marginBottom: "1rem" }}
-                error={
-                  formik.touched.password &&
-                  Boolean(formik.errors.password)
-                }
-                helperText={
-                  formik.touched.password && formik.errors.password
-                }
-              />
+        <Grid
+          container
+          item
+          xs={10}
+          sm={10}
+          md={10}
+          sx={{
+            color: "white",
+            borderRadius: "10px",
+            width: "550px",
+            height: "500px",
+            padding: "1rem",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            position: "relative",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+          }}
+        >
+          <IconButton
+            onClick={openModal}
+            color="primary"
+            sx={{
+              position: "absolute",
+              top: 10,
+              right: -10,
+              zIndex: 2,
+            }}
+          >
+            <RecentActorsIcon sx={{ fontSize: 40, mt: -5 }} />
+            <Typography sx={{ position: "absolute", mt: 5 }}>
+              Usuarios
+            </Typography>
+          </IconButton>
 
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                fullWidth
-                style={{ marginTop: 3, height: "60px" }}
+          <Modal
+            open={isModalOpen}
+            onClose={closeModal}
+            aria-labelledby="modal-title"
+            aria-describedby="modal-description"
+          >
+            <Box
+              sx={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                marginLeft: -1,
+                transform: "translate(-50%, -50%)",
+                width: "auto",
+                bgcolor: "background.paper",
+                borderRadius: 2,
+                boxShadow: 24,
+                p: 4,
+                backgroundColor: "#0072B9",
+                color: "white",
+                textAlign: "center",
+              }}
+            >
+              <IconButton
+                onClick={closeModal}
+                sx={{
+                  width: "25px",
+                  height: "25px",
+                  border: 1,
+                  color: "white",
+                  position: "absolute",
+                  top: 5,
+                  right: 5,
+                  zIndex: 2,
+                }}
               >
-                Iniciar Sesión
-              </Button>
+                <CloseIcon sx={{ fontSize: 18 }} />
+              </IconButton>
+              <Typography
+                id="modal-title"
+                component="h2"
+                sx={{
+                  padding: 1,
+                  mt: -2,
+                  fontSize: 18,
+                  backgroundColor: "rgba(0, 0, 0, 0.7)",
+                  borderRadius: 2,
+                }}
+              >
+                En esta sección podrás elegir un usuario de prueba para ingresar
+              </Typography>
+              <Typography
+                id="modal-title"
+                variant="h6"
+                component="h2"
+                sx={{ mb: -2, textDecoration: "underline" }}
+              >
+                Usuarios / Djs Users
+              </Typography>
+              <ul style={{ listStyle: "none" }}>
+                {djUsers.map((djUser) => (
+                  <li
+                    key={djUser.userEmail}
+                    onClick={() => handleUserSelection(djUser)}
+                    style={{
+                      marginLeft: -40,
+                      padding: 8,
+                      cursor: "pointer",
+                      marginBottom: 10,
+                      backgroundColor: "rgba(0, 0, 0, 0.7)",
+                      borderRadius: 10,
+                    }}
+                  >
+                    <Typography
+                      sx={{ fontSize: 12 }}
+                    >{`Email: ( ${djUser.userEmail}), Contraseña: (${djUser.userPassword})`}</Typography>
+                  </li>
+                ))}
+              </ul>
 
-              <Button
-                variant="outlined"
-                color="primary"
-                fullWidth
-                onClick={onClose}
-                style={{ marginTop: 25, height: "60px" }}
+              <Typography
+                id="modal-title"
+                variant="h6"
+                component="h2"
+                sx={{ mt: 4, mb: -2, textDecoration: "underline" }}
               >
-                Registrarse aquí
-              </Button>
-            </FormControl>
-          </form>
+                Usuarios / Music Users
+              </Typography>
+              <ul style={{ listStyle: "none" }}>
+                {musicUsers.map((musicUser) => (
+                  <li
+                    key={musicUser.userEmail}
+                    onClick={() => handleUserSelection(musicUser)}
+                    style={{
+                      marginLeft: -40,
+                      padding: 8,
+                      cursor: "pointer",
+                      marginBottom: 10,
+                      backgroundColor: "rgba(0, 0, 0, 0.7)",
+                      borderRadius: 10,
+                    }}
+                  >
+                    <Typography
+                      sx={{ fontSize: 12 }}
+                    >{`Email: (${musicUser.userEmail}), Contraseña: (${musicUser.userPassword})`}</Typography>
+                  </li>
+                ))}
+              </ul>
+            </Box>
+          </Modal>
+          <Typography
+            variant="h4"
+            gutterBottom
+            sx={{
+              color: "white",
+              backgroundColor: "rgba(0, 0, 0, 0.8)",
+              mt: -5,
+              mb: 10,
+            }}
+          >
+            Iniciar Sesión
+          </Typography>
+          <Grid sx={{ width: "90%" }}>
+            <form onSubmit={formik.handleSubmit}>
+              <FormControl style={{ width: "100%" }}>
+                <Typography mb={-2}>Email</Typography>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  size="small"
+                  margin="normal"
+                  type="email"
+                  {...formik.getFieldProps("email")}
+                  sx={{ backgroundColor: "white", marginBottom: "1rem" }}
+                  error={formik.touched.email && Boolean(formik.errors.email)}
+                  helperText={formik.touched.email && formik.errors.email}
+                />
+                {formik.touched.email && formik.errors.email && (
+                  <Typography variant="caption" color="red" mt={-2} mb={2}>
+                    {formik.errors.email}
+                  </Typography>
+                )}
+                <Typography mb={-2}>Contraseña</Typography>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  size="small"
+                  margin="normal"
+                  type="password"
+                  {...formik.getFieldProps("password")}
+                  sx={{ backgroundColor: "white", marginBottom: "1rem" }}
+                  error={
+                    formik.touched.password && Boolean(formik.errors.password)
+                  }
+                  helperText={formik.touched.password && formik.errors.password}
+                />
+                {formik.touched.password && formik.errors.password && (
+                  <Typography variant="caption" color="red" mt={-2} mb={2}>
+                    {formik.errors.password}
+                  </Typography>
+                )}
+
+                <Button
+                  type="submit"
+                  variant="outlined"
+                  color="primary"
+                  fullWidth
+                  style={{
+                    marginTop: 3,
+                    height: "60px",
+                    backgroundColor: "rgba(0, 128, 255, 0.5)",
+                    color: "white",
+                    borderColor: "black",
+                  }}
+                  sx={{
+                    transition: "transform 0.3s ease-in-out",
+                    "&:hover": {
+                      transform: "scale(1.2)",
+                    },
+                  }}
+                >
+                  Iniciar Sesión
+                </Button>
+
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  fullWidth
+                  onClick={onClose}
+                  style={{
+                    marginTop: 30,
+                    height: "60px",
+                    backgroundColor: "rgba(0, 128, 255, 0.5)",
+                    color: "white",
+                    borderColor: "black",
+                  }}
+                  sx={{
+                    transition: "transform 0.3s ease-in-out",
+                    "&:hover": {
+                      transform: "scale(1.2)",
+                    },
+                  }}
+                >
+                  Registrarse aquí
+                </Button>
+              </FormControl>
+            </form>
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 };
 

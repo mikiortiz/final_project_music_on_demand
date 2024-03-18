8d83f4cd4180b4efc4271982b67b7c11e2c850d9
Miguel Ortiz <ortizmichel390@gmail.com>
2024-03-18 14:41:00 -0300

    changes-in-main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,17 @@ import Store from "./redux/Store";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider store={Store}>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <App />
-      </LocalizationProvider>
-    </Provider>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <Provider store={Store}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <App />
+        </LocalizationProvider>
+      </Provider>
+    </React.StrictMode>
+  );
+} else {
+  console.error("No se encontró el elemento con ID 'root'");
+}

c4bc33731ef23dcf6825f493ddcf3f7d460b4acd
Miguel Ortiz <ortizmichel390@gmail.com>
2025-02-09 13:37:01 -0300

    The layout of texts in the banner and font sizes have been improved

--- src/components/NotificationsBanner.tsx
+++ src/components/NotificationsBanner.tsx
@@ -5,21 +5,35 @@ const NotificationBanner = () => {
     <Box
       sx={{
         width: "100%",
-        backgroundColor: "#FF5733", // Color amarillo-rojo
+        backgroundColor: "#FF5733",
         padding: "10px",
         textAlign: "center",
         position: "fixed",
-        bottom: 0, // Moverlo a la parte inferior
+        bottom: 0,
         left: 0,
         zIndex: 1000,
       }}
     >
-      <Typography variant="body1" fontWeight="bold">
-        Hey, tenemos un problema con Spotify. Actualmente, están realizando
-        actualizaciones en su API, lo que impide procesar solicitudes desde
-        nuestra app. Debido a esto, los géneros y albumes no se están
-        mostrando. Estamos trabajando en una solución. ¡Gracias
-        por la paciencia!
+      <Typography
+        variant="h6"
+        fontWeight="bold"
+        sx={{ marginBottom: "2px", marginTop: "-10px" }}
+      >
+        Hey, tenemos un problema con Spotify
+      </Typography>
+      <Typography
+        variant="body2"
+        sx={{ color: "black", lineHeight: 1.5, textAlign: "center" }}
+      >
+        Actualmente, están realizando actualizaciones en su API, lo que impide
+        procesar solicitudes desde nuestra app. Debido a esto, los géneros y
+        álbumes no se están mostrando.!
+      </Typography>
+      <Typography
+        variant="body2"
+        sx={{ color: "black", lineHeight: 1.5, textAlign: "center" }}
+      >
+        Estamos trabajando en una solución. ¡Gracias por la paciencia!
       </Typography>
     </Box>
   );
